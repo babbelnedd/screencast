dca2abe36acfbfe957edb71456880e85e5afdc40
SchadLucas <schad.lucas@gmail.com>
2014-01-13 01:17:03 +0100

    added resume dialog

--- screencast_pause.sh
+++ screencast_pause.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 killall ffmpeg
-notify-send -t 6000 "Screencast paused"
+#notify-send -t 6000 "Screencast paused"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+zenity --question --text "Would you like to resume?"
+if [ $? = 0 ];then
+# 'yes'
+bash $DIR/screencast.sh
+else
+# 'no'
+notify-send -t 6000 "Screencast stopped"
+fi
+
 #killall key-mon
